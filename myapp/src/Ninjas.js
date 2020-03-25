2ba2288f5f86be91f73bbda63a2f7a0583bc6d7c
Keith Skinner <KeithSkinner93@gmail.com>
2020-03-25 14:45:59 -0700

    Conditional rendering

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,14 +1,18 @@
 import React from "react";
 
+const RenderNinja = (ninja) => {
+    return (
+        <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+        </div>
+    )
+}
+
 const Ninjas = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-        )
+        return ninja.age > 20 ? RenderNinja(ninja) : null
     })
     return (
         <div className="ninja-list">
