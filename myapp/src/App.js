547288dc079d4add591e2b66a406dd7cac9e6642
Keith Skinner <KeithSkinner93@gmail.com>
2020-03-25 14:02:49 -0700

    Creating/Rendering Sub components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Ninjas from './Ninjas'
 
 function App() {
   return (
     <div className="App">
       <h1>My first react app!</h1>
       <p>Welcome :)</p>
+      <Ninjas/>
     </div>
   );
 }
 
-export default App;
+export default App
