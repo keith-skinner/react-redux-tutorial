2a140a51cfe97ff237010c92faf052a45feee0a2
Keith Skinner <KeithSkinner93@gmail.com>
2020-03-25 14:36:29 -0700

    Creating lists of components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Ninjas from './Ninjas'
 
-function App() {
+function App(props, state) {
+  const [ninjas] = useState([
+    { name: 'Ryu', age:30, belt: 'black', id:1 },
+    { name: 'Yoshi', age:20, belt: 'green', id:2 },
+    { name: 'Crystal', age:25, belt: 'pink', id:3 },
+  ])
   return (
     <div className="App">
       <h1>My first react app!</h1>
       <p>Welcome :)</p>
-      <Ninjas name="Ryu" age="25" belt="black" />
-      <Ninjas name="Yoshi" age="30" belt="green" />
+      <Ninjas ninjas={ninjas}/>
     </div>
   );
 }
