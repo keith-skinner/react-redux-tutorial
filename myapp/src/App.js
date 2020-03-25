db9c501eda2851011dc28685ff749ba0601398aa
Keith Skinner <KeithSkinner93@gmail.com>
2020-03-25 14:37:33 -0700

    Stateless/Functional Components

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import Ninjas from './Ninjas'
 
-function App(props, state) {
-  const [ninjas] = useState([
-    { name: 'Ryu', age:30, belt: 'black', id:1 },
-    { name: 'Yoshi', age:20, belt: 'green', id:2 },
-    { name: 'Crystal', age:25, belt: 'pink', id:3 },
-  ])
-  return (
-    <div className="App">
-      <h1>My first react app!</h1>
-      <p>Welcome :)</p>
-      <Ninjas ninjas={ninjas}/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age:30, belt: 'black', id:1 },
+      { name: 'Yoshi', age:20, belt: 'green', id:2 },
+      { name: 'Crystal', age:25, belt: 'pink', id:3 },
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My first react app!</h1>
+        <p>Welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    )
+  }
 }
 
 export default App
